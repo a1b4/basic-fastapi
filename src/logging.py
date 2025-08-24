8238f637d1c19551d7969521da05ff2776a7d55e
Alba Cárdenas <albacardenas.andrade@gmail.com>
2025-08-24 13:04:02 +0200

    chore: add Flake8 and fix style issues

--- src/logging.py
+++ src/logging.py
@@ -19,9 +19,9 @@ def configure_logging(log_level: str = LogLevels.error):
     if log_level not in log_levels:
         logging.basicConfig(level=LogLevels.error)
         return
-    
+
     if log_level == LogLevels.debug:
         logging.basicConfig(level=log_level, format=LOG_FORMAT_DEBUG)
         return
-    
+
     logging.basicConfig(level=log_level)
